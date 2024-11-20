baf0db8c7ba6ca110767ab318c5cdbadef720148
Rciat05 <rciglesiaa05@gmail.com>
2024-11-19 21:10:49 -0600

    Agregando el proyecto

---
+++ ZonaRTX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZonaRTX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
